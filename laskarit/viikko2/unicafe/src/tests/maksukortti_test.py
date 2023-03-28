eadbae310465b0392668a032c4b268a5e134eb70
Reetta Himanka <reetta.himanka@gmail.com>
2023-03-28 15:45:57 +0300

    Added unicafe maksukortti tests

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,23 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_on_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_lataaminen_kasvattaa_saldo_oikein(self):
+        self.maksukortti.lataa_rahaa(200)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 12.00 euroa")
+
+    def test_saldo_vahenee_oikein(self):
+        self.maksukortti.ota_rahaa(200)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 8.00 euroa")
+
+    def test_saldo_ei_muutu_jos_rahaa_ei_ole_tarpeeksi(self):
+        kortti = Maksukortti(500)
+        kortti.ota_rahaa(600)
+        self.assertEqual(str(kortti), "Kortilla on rahaa 5.00 euroa")
+
+    def test_ota_rahaa_antaa_oikean_totuusarvon(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1200), False)
+        self.assertEqual(self.maksukortti.ota_rahaa(500), True)
